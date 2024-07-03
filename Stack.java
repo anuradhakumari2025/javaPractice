72d3bb3cd6b55901d8978e0db52fb49edff076b5
anuradhakumari2025 <krianuradha4321@gmail.com>
2024-07-03 16:16:05 +0530

    Added Stack

---
+++ Stack.java
@@ -0,0 +1,62 @@
+import java.util.NoSuchElementException;
+
+public class Stack {
+
+  ListNode top;
+  int length;
+
+  class ListNode {
+    int data;
+    ListNode next;
+
+    ListNode(int data) {
+      this.data = data;
+    }
+  }
+
+  public int length() {
+    return length;
+  }
+
+  public boolean isEmpty() {
+    return length == 0;
+  }
+
+  public void push(int data) {
+    ListNode temp = new ListNode(data);
+    temp.next = top;
+    top = temp;
+    length++;
+  }
+
+  public int pop() {
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    }
+    int result = top.data;
+    top = top.next;
+    length--;
+    return result;
+  }
+
+  public int peek() {
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    }
+    return top.data;
+  }
+
+  public static void main(String[] args) {
+    Stack stack = new Stack();
+    stack.push(8);
+    stack.push(5);
+    stack.push(15);
+    stack.push(50);
+
+    stack.pop();
+    stack.pop();
+    stack.pop();
+    System.out.println(stack.peek());
+
+  }
+}
